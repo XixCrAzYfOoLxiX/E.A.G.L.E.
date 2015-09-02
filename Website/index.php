acc120895c81c86f202e6fb013b701d48a4e1f21
Altibond <james.e.hart@gmail.com>
2015-09-02 20:58:29 +0100

    Fiddled with it

--- Website/index.php
+++ Website/index.php
@@ -2,10 +2,8 @@
     <title>Welcome to E.A.G.L.E.</title>
 </head>
 <body>
-<<<<<<< HEAD
    <h1> Jet fuel can't melt dank memes. </h1> 
 </body>
-=======
-    
+=======  
 </body>
 >>>>>>> origin/master
